32916ad28d557c0050ce7bae5cba71f5bdb5d26e
RWB0104 <psj2716@gmail.com>
2025-09-01 08:56:38 +0000

    release: 🚀 @kapoo/piedit v3.3.4

---
+++ docs/_next/static/PQAG2oMVrxTtYuL2MI_fC/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/f5ff9a1b4f2ab9be.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
